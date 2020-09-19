96f7f477f9142c03c79b854b893ede2090be43c1
dmalicia <dmalicia@gmail.com>
2020-09-19 18:42:18 -0400

    module

--- variables.tf
+++ variables.tf
@@ -2,3 +2,16 @@ variable gcp_zones {
   description = "A list of GCP zone to launch in"
   type        = list
 }
+# Source code folder
+variable "scbootstrap" {
+  type       = string
+  default    = "/usr/local/share/dm-terraform/frontend/bootstrap/"
+}
+
+# Seed Creds
+variable "scpath" {
+  type       = string
+  default    = "/usr/local/share/dm-terraform/seed/bootstrap/"
+}
+
+
