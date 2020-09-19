1c7421e64ca6fdb15f1d9ff52320a2e596128dd0
dmalicia <dmalicia@gmail.com>
2020-09-19 18:44:24 -0400

    module

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "frontend" {
  count        = length(var.gcp_zones) 
  name         = "dmlc-frontend-${random_id.instance_id.hex}"
  machine_type = "f1-micro"
- zone         = var.gcp_zones
+ zone         = var.gcp_zones[count.index]
 
  boot_disk {
    initialize_params {
