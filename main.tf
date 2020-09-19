1eea6d6b9f8d31907a92edf96dd347c1503b75a9
dmalicia <dmalicia@gmail.com>
2020-09-19 18:23:39 -0400

    module

---
+++ main.tf
@@ -0,0 +1,35 @@
+resource "google_compute_instance" "frontend" {
+ count        = lenght(var.gcp_zones) 
+ name         = "dmlc-frontend-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = var.gcp_zones
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+metadata_startup_script = file("${var.scbootstrap}/puppet.sh")
+ metadata = {
+   ssh-keys = "dmalicia:${file("${var.scpath}/id_rsa.pub")}"
+            }
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     nat_ip = google_compute_address.static[count.index].address
+   }
+ }
+}
+
+resource "google_compute_firewall" "frontend" {
+ name    = "frontend-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["80","8140"]
+ }
+}
